5515b89e4504703d82d806f3a3fb1a7659afac94
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-10-17 13:48:15 +0800

    🚧 CI

---
+++ get_jb_licenses.py
@@ -0,0 +1,59 @@
+import json
+import os
+import re
+import requests
+
+from lxml import etree
+
+
+class cex(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def get_raw_html() -> str:
+    url = 'https://ipfs.io/ipfs/bafybeih65no5dklpqfe346wyeiak6wzemv5d7z2ya7nssdgwdz4xrmdu6i/'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    r = requests.get(url=url,
+                     headers={
+                         'User-Agent': user_agent
+                     })
+    if 200 == r.status_code:
+        return r.text
+    else:
+        raise cex(f'http cod is {r.status_code}')
+
+
+def get_licenses(raw_html: str) -> []:
+    def get_jbkeys(raw_html: str) -> {}:
+        pattern = r'\s+let\sjbKeys\s='
+        for line in raw_html.splitlines():
+            if re.match(pattern, line):
+                return json.loads(re.sub(pattern, '', line)[:-1].strip())
+
+    jbkeys = get_jbkeys(raw_html)
+    x_dom = etree.HTML(raw_html)
+    articles = x_dom.xpath('//article[@data-sequence]')
+    for article in articles:
+        data_sequence = article.get('data-sequence')
+        data_title = article.xpath('./descendant::h1')[0].get('title')
+        data_version = article.xpath('./descendant::button[@data-version]')[0].get('data-version')
+        yield [f'{re.sub(r'[^\w\-.]', '_', data_title)}.{data_version}.license',
+               jbkeys.get(data_sequence).get(data_version)]
+
+
+def write_licenses(raw_html) -> None:
+    jb_licenses = get_licenses(raw_html)
+    os.makedirs('licenses', exist_ok=True)
+    for jb_license in jb_licenses:
+        with open(f'licenses/{jb_license[0]}', 'w') as f:
+            f.write(jb_license[1])
+
+
+def execute() -> None:
+    raw_html = get_raw_html()
+    write_licenses(raw_html)
+
+
+if __name__ == '__main__':
+    execute()
